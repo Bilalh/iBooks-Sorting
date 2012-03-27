4b7c41b0c6f1b587e00427a7b0e419fb36cfd281
Bilal Syed Hussain <bhterra@gmail.com>
2012-03-27 04:19:55 +0100

    sorting  by field

--- books_lib.rb
+++ books_lib.rb
@@ -1,6 +1,11 @@
+Mapping ={
+	:author     => "b.ZSORTAUTHOR",
+	:title      => "b.ZBOOKTITLE",
+	:sort_title => "b.ZSORTTITLE"
+}
 
-def error_handing(args, usage)
-   if args.length != 2
+def error_handing(args, usage,strict=false)
+   if  (strict && args.length != 2) || args.length < 2
      raise "Usage  #{usage}"
    end
 	
@@ -14,6 +19,15 @@ def error_handing(args, usage)
 end
 
 
+def parse_subsort(args)
+	fields=[]
+	args.each do |e|
+		f = e.to_sym
+		raise "Invalid sort field #{e}" unless Mapping.has_key? f
+		fields << f
+	end
+	fields
+end
 
 class Books
 	require 'sqlite3'
@@ -37,40 +51,51 @@ class Books
 		
 	end
 	
-	def sort_collection(collection_id, field)
-		puts "#{collection_id}, #{field}"
+	def sort_collection(collection_id, fields_arr)
+		puts "Sorting #{collection_id}, by #{fields_arr}"
+		@db.results_as_hash = true
+		
+		
+		fields_arr.map! do |e|
+			Mapping[e]
+		end
+		fields     = fields_arr.join ','
+		
 		query = <<-SQL
-		Select  b.Z_PK, b.ZSORTTITLE ,b.ZSORTKEY, ZSORTAUTHOR
+		Select  b.Z_PK, b.ZSORTTITLE ,b.ZSORTKEY, ZSORTAUTHOR, b.ZBOOKTITLE
 		from ZBKCOLLECTIONMEMBER c
 		Join ZBKBOOKINFO  b on c.ZDATABASEKEY = b.ZDATABASEKEY
 		Join ZBKCOLLECTION cc on cc.Z_PK = c.ZCOLLECTION
 		where cc.Z_PK='#{collection_id}'
-		Order by b.ZSORTAUTHOR, b.ZSORTTITLE
+		Order by #{fields}
 		SQL
 		
 		results = @db.execute query
 		
 		# Get the sort keys highest first
 		numbers  = results.collect do |e|
-			e[2]
+			e['ZSORTKEY']
 		end.sort.reverse
 		
 		# pp numbers
 		
 		# set the sort keys
 		results.each_with_index do |e, i|
-			e[2]= numbers[i]
+			e['ZSORTKEY']= numbers[i]
 		end
 		
-		puts
-		pp results
+		puts "Highest sortkey appears first"
+		results.each do |e|
+			puts "%10d %-25s %s" % [e['ZSORTKEY'], e['ZSORTAUTHOR'], e['ZSORTTITLE']]
+			puts "%10d %-25s %s" % [e['ZSORTKEY'], e['ZSORTAUTHOR'], e['ZBOOKTITLE']]
+		end
 		
 		# Update
 		results.each do |e|
 			puts @db.execute(
 				"Update ZBKBOOKINFO
-				 Set ZSORTKEY = #{e[2]}
-				 where Z_PK   = #{e[0]}"
+				 Set ZSORTKEY = #{e['ZSORTKEY']}
+				 where Z_PK   = #{e['Z_PK']}"
 			)
 		end
 	end
